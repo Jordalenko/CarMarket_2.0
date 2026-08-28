43054eb9d370d48507348e6055ae93037993d47b
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2026-08-28 19:28:52 +0100

    Enable production debug settings

--- CarMarket/settings.py
+++ CarMarket/settings.py
@@ -31,10 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*.herokuapp.com'
     'car-market-2-0-88117026bcc0.herokuapp.com',
     '127.0.0.1',
     'localhost',]
